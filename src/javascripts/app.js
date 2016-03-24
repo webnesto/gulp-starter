fac06a24b3333644fb5dcb461b1b9fad8093f747
Dan Tello <dan.tello@viget.com>
2016-03-24 12:54:41 -0400

    DEMO

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,19 @@
-import './modules'
+import './asyncModules'
+import exclaimify from './exclaimify'
 
-console.log(`app.js has loaded!`)
+const button = document.getElementById('button')
+
+const alertAsyncMessage = function() {
+  // CommonJS async syntax webpack magic
+  require.ensure([], function() {
+    const message = require("./asyncMessage").default
+    alert(exclaimify(message))
+  })
+}
+
+console.log(`
+  asset references like this one:
+    images/gulp.png
+  get updated in js too!`)
+
+button.addEventListener('click', alertAsyncMessage)
